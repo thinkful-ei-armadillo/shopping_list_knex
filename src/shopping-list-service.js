4066c80643ff64a2f121dab3a5abfe3858cdc61b
AReach <DerAReich@gmail.com>
2019-02-26 14:25:02 -0600

    completed shopping-list-service CRUD operations, began tests in shopping-list-service.spec.js

--- src/shopping-list-service.js
+++ src/shopping-list-service.js
@@ -1,6 +1,34 @@
 const ShoppingListService = {
   getAllItems(knex) {
-    return 'all the items!';
+    return knex
+      .select('*')
+      .from('shopping_list');
+  },
+  insertItem(knex, newItem) {
+    return knex
+      .insert(newItem)
+      .into('shopping_list')
+      .returning('*')
+      .then(rows => rows[0]);
+  },
+  getItemById(knex, id) {
+    return knex
+      .select('*')
+      .from('shopping_list')
+      .where('id', id)
+      .first();
+  },
+  updateItem(knex, id, newItem) {
+    return knex
+      .from('shopping_list')
+      .where({ id })
+      .update(newItem);
+  },
+  deleteItem(knex, id) {
+    return knex
+      .from('shopping_list')
+      .where({ id })
+      .delete();
   }
 };
 
---
+++ test/shopping-list-service.spec.js
@@ -0,0 +1,91 @@
+const ShoppingListService = require('../src/shopping-list-service');
+const knex = require('knex');
+
+/* global expect */
+
+describe('Shopping list CRUD tests', () => {
+  let db;
+  let testItems = [
+    {
+      id: 1,
+      name: 'Steak',
+      date_added: new Date('2019-02-26T20:06:28.140Z'),
+      checked: false,
+      price: '0.01',
+      category: 'Main'
+    },
+    {
+      id: 2,
+      name: 'Steak 2',
+      date_added: new Date('2019-02-26T20:06:28.140Z'),
+      checked: false,
+      price: '6.01',
+      category: 'Lunch'
+    },
+    {
+      id: 3,
+      name: 'Steak 3',
+      date_added: new Date('2019-02-26T20:06:28.140Z'),
+      checked: false,
+      price: '0.23',
+      category: 'Main'
+    },
+    {
+      id: 4,
+      name: 'Steak 4',
+      date_added: new Date('2019-02-26T20:06:28.140Z'),
+      checked: false,
+      price: '88.01',
+      category: 'Snack'
+    }
+  ];
+
+  before(() => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.DB_URL_TEST
+    });
+  });
+
+  before(() => db('shopping_list').truncate());
+
+  afterEach(() => db('shopping_list').truncate());
+
+  after(() => db.destroy());
+
+  context(`Given 'shopping_list' has data`, () => {
+    beforeEach(() => {
+      return db
+        .into('shopping_list')
+        .insert(testItems);
+    });
+
+    it('getAllItems() resolves all items from the table', () => {
+      return ShoppingListService.getAllItems(db)
+        .then(actual => {
+          expect(actual).to.eql(testItems);
+        });
+    });
+  });
+
+  it('insertItem() inserts an item and resolves the item', () => {
+    const newItem = {
+      name: 'Test item',
+      price: '5.55',
+      date_added: new Date('2019-02-26T20:06:28.140Z'),
+      checked: false,
+      category: 'Snack'
+    };
+    return ShoppingListService.insertItem(db, newItem)
+      .then(actual => {
+        expect(actual).to.eql({
+          id: 1,
+          name: newItem.name,
+          price: newItem.price,
+          date_added: newItem.date_added,
+          checked: newItem.checked,
+          category: newItem.category
+        });
+      });
+  });
+});
